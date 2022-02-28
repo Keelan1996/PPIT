89b9ec2a1e4095d986963d42aca8cc6b81acbd69
Keelan1996 <liverpoolkeelan@gmail.com>
2022-02-28 00:19:16 +0000

    set website skeleton

---
+++ components/Navbar.jsx
@@ -0,0 +1,15 @@
+import styles from "../styles/Navbar.module.css"
+
+
+
+const Navbar = () =>{
+    return(
+        <div className= {styles.container}>
+           navbar
+        </div>
+
+    )
+}
+
+
+export default Navbar
